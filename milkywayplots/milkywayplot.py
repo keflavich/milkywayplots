7e514cc677cc03daf2bb5305b86076a87e075624
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2013-11-03 17:28:42 +0100

    first real commit: added generalized version of "Bound HII" plotter and Hurt plot

---
+++ milkywayplots/milkywayplot.py
@@ -0,0 +1,167 @@
+import numpy as np
+import PIL
+
+import matplotlib.pyplot as plt
+
+from mpl_toolkits.axisartist.grid_helper_curvelinear import GridHelperCurveLinear
+
+from mpl_toolkits.axisartist import SubplotHost, ParasiteAxesAuxTrans
+
+import mpl_toolkits.axisartist.angle_helper as angle_helper
+from matplotlib.projections import PolarAxes
+from matplotlib.transforms import Affine2D
+
+# annotated version
+# http://upload.wikimedia.org/wikipedia/commons/8/89/236084main_MilkyWay-full-annotated.jpg
+
+import urllib
+
+def get_image(mw_img_url="http://upload.wikimedia.org/wikipedia/commons/0/09/Milky_Way_2005.jpg"):
+    """
+    Download an image - default is non-annotated Robert Hurt image
+
+    Annotated version here:
+    http://upload.wikimedia.org/wikipedia/commons/8/89/236084main_MilkyWay-full-annotated.jpg
+    """
+    urllib.urlretrieve(mw_img_url)
+
+def make_mw_plot(fig=None, mw_img_name = "Milky_Way_2005.jpg",
+        solar_rad=8.5, fignum=5):
+    """
+    Generate a "Milky Way" plot with Robert Hurt's Milky Way illustration as
+    the background.
+
+    .. TODO:
+        Figure out how to fix the axis labels.  They don't work now!
+
+    Parameters
+    ----------
+    fig : matplotlib.figure instance
+        If you want to start with a figure instance, can specify it
+    mw_img_name: str
+        The name of the image on disk
+    solar_rad : float
+        The assumed Galactocentric orbital radius of the sun
+    fignum : int
+        If Figure not specified, use this figure number
+    """
+
+    # load image
+    mw = np.array(PIL.Image.open(mw_img_name))[:,::-1]
+
+    # set some constants
+    npix = mw.shape[0] # must be symmetric
+    # Galactic Center in middle of image
+    gc_loc = [x/2 for x in mw.shape]
+
+    # Sun is at 0.691 (maybe really 0.7?) length of image
+    sun_loc = mw.shape[0]/2,int(mw.shape[1]*0.691)
+    # determine scaling
+    kpc_per_pix = solar_rad / (sun_loc[1]-gc_loc[1])
+    boxsize = npix*kpc_per_pix
+
+    # most of the code below is taken from:
+    # http://matplotlib.sourceforge.net/examples/axes_grid/demo_curvelinear_grid.html
+    # and http://matplotlib.sourceforge.net/examples/axes_grid/demo_floating_axis.html
+
+    if fig is None:
+        fig = plt.figure(fignum)
+    plt.clf()
+
+    # PolarAxes.PolarTransform takes radian. However, we want our coordinate
+    # system in degree
+    # this defines the polar coordinate system @ Galactic center
+    tr = Affine2D().scale(np.pi/180., 1.) + PolarAxes.PolarTransform()
+
+    # polar projection, which involves cycle, and also has limits in
+    # its coordinates, needs a special method to find the extremes
+    # (min, max of the coordinate within the view).
+
+    # grid helper stuff, I think (grid is off by default)
+    # This may not apply to the image *at all*, but would if you
+    # used the grid
+    # 40, 40 : number of sampling points along x, y direction
+    extreme_finder = angle_helper.ExtremeFinderCycle(40, 40,
+                                                     lon_cycle = 360,
+                                                     lat_cycle = None,
+                                                     lon_minmax = None,
+                                                     lat_minmax = (0, np.inf),
+                                                     )
+
+    grid_locator1 = angle_helper.LocatorDMS(12)
+    # Find a grid values appropriate for the coordinate (degree,
+    # minute, second).
+
+    tick_formatter1 = angle_helper.FormatterDMS()
+    # And also uses an appropriate formatter.  Note that,the
+    # acceptable Locator and Formatter class is a bit different than
+    # that of mpl's, and you cannot directly use mpl's Locator and
+    # Formatter here (but may be possible in the future).
+
+    grid_helper = GridHelperCurveLinear(tr,
+                extreme_finder=extreme_finder,
+                grid_locator1=grid_locator1,
+                tick_formatter1=tick_formatter1,
+                #tick_formatter2=matplotlib.ticker.FuncFormatter(lambda x: x * kpc_per_pix)
+                )
+
+
+    ax = SubplotHost(fig, 1, 1, 1, grid_helper=grid_helper, axisbg='#333333')
+    fig.add_subplot(ax)
+    # ax.transData is still a (rectlinear) pixel coordinate. Only the
+    # grids are done in galactocentric coordinate.
+
+    # show the image
+    ax.imshow(mw,extent=[-boxsize/2,boxsize/2,-boxsize/2,boxsize/2])
+
+    ax_pixgrid = ax.twin() # simple twin will give you a twin axes,
+                           # but with normal grids.
+
+    # to draw heliocentric grids, it is best to update the grid_helper
+    # with new transform.
+
+    # need to rotate by -90 deg to get into the standard convention
+    tr_helio = Affine2D().scale(np.pi/180., 1.).translate(-np.pi/2.,0) + \
+               PolarAxes.PolarTransform() + \
+               Affine2D().translate(0,solar_rad)
+    # Note that the transform is from the heliocentric coordinate to
+    # the pixel coordinate of ax (i.e., ax.transData).
+
+    ax.get_grid_helper().update_grid_finder(aux_trans=tr_helio)
+
+    # Now we defina parasite axes with galactocentric & heliocentric
+    # coordinates.
+
+    # A parasite axes with given transform
+    gc_polar = ParasiteAxesAuxTrans(ax, tr, "equal")
+    ax.parasites.append(gc_polar)
+    # note that ax2.transData == tr + galactocentric_axis.transData
+    # Anthing you draw in ax2 will match the ticks and grids of galactocentric_axis.
+
+    hc_polar = ParasiteAxesAuxTrans(ax, tr_helio, "equal")
+    ax.parasites.append(hc_polar)
+
+
+    return ax, ax_pixgrid, gc_polar, hc_polar
+
+if __name__=="__main__":
+    ax, ax_pixgrid, gcp,hcp = make_mw_plot()
+    ax.grid(color="w")
+    ax_pixgrid.grid(color="r")
+    ax_pixgrid.axis[:].major_ticklabels.set_color("r")
+    ax.set_autoscale_on(False)
+
+    # hcp.grid(color='white')
+    hcp.plot(0,0,'o',color='gold',markersize=10,markeredgecolor='gold',markerfacecolor='none',zorder=50,alpha=1,markeredgewidth=2)
+    hcp.plot(0,0,'.',color='gold',markersize=3,markeredgecolor='gold',markerfacecolor='gold',zorder=50,alpha=1,markeredgewidth=2)
+    ax.plot(0,0,'o',color=(0.3,1,0.3,0.5),markersize=30,markeredgecolor=(0.3,1,0.3,0.5))
+    ax.text(0,0,'GC',horizontalalignment='center',verticalalignment='center')
+
+    gcp.plot(-30,3,'s')
+    hcp.plot(20,3,'^')
+    hcp.plot(45,6,'h')
+    hcp.plot(-45,6,'o')
+    hcp.plot(np.linspace(0,90),np.ones(50)*3)
+    hcp.plot(np.linspace(90,180),np.ones(50)*4)
+
+    plt.show()
---
+++ milkywayplots/polar_galactic_plot.py
@@ -0,0 +1,190 @@
+import matplotlib.pyplot as plt
+import matplotlib.cbook as cbook
+import matplotlib
+from matplotlib.patches import Circle,Wedge
+import numpy as np
+
+from  mpl_toolkits.axisartist.grid_helper_curvelinear import GridHelperCurveLinear
+from mpl_toolkits.axisartist import Subplot
+
+from mpl_toolkits.axisartist import SubplotHost, ParasiteAxesAuxTrans
+
+import  mpl_toolkits.axisartist.angle_helper as angle_helper
+from matplotlib.projections import PolarAxes
+from matplotlib.transforms import Affine2D
+
+
+def make_polar_axis(figure):
+    """
+    Generate a polar axis.
+
+    Examples
+    --------
+    >>> from pylab import *
+    >>> f = figure()
+    >>> ax1,ax2 = make_polar_axis(f)
+    >>> f.add_subplot(ax1)
+    """
+    # PolarAxes.PolarTransform takes radian. However, we want our coordinate
+    # system in degree
+    tr = Affine2D().scale(np.pi/180., 1.) + PolarAxes.PolarTransform()
+
+    # polar projection, which involves cycle, and also has limits in
+    # its coordinates, needs a special method to find the extremes
+    # (min, max of the coordinate within the view).
+
+    # 20, 20 : number of sampling points along x, y direction
+    extreme_finder = angle_helper.ExtremeFinderCycle(40, 40,
+                                                     lon_cycle=360,
+                                                     lat_cycle=None,
+                                                     lon_minmax=None,
+                                                     lat_minmax=(0, np.inf),
+                                                     )
+
+    grid_locator1 = angle_helper.LocatorDMS(12)
+    # Find a grid values appropriate for the coordinate (degree,
+    # minute, second).
+
+    tick_formatter1 = angle_helper.FormatterDMS()
+    # And also uses an appropriate formatter.  Note that,the
+    # acceptable Locator and Formatter class is a bit different than
+    # that of mpl's, and you cannot directly use mpl's Locator and
+    # Formatter here (but may be possible in the future).
+
+    grid_helper = GridHelperCurveLinear(tr,
+                                        extreme_finder=extreme_finder,
+                                        grid_locator1=grid_locator1,
+                                        tick_formatter1=tick_formatter1,
+                                        #tick_formatter2=matplotlib.ticker.FuncFormatter(lambda x: x * kpc_per_pix)
+                                        )
+
+
+    ax1 = SubplotHost(figure, 1, 1, 1, grid_helper=grid_helper, axisbg='#333333')
+
+    # make ticklabels of right and top axis visible.
+    ax1.axis["right"].major_ticklabels.set_visible(True)
+    ax1.axis["top"].major_ticklabels.set_visible(True)
+
+    # let right axis shows ticklabels for 1st coordinate (angle)
+    ax1.axis["right"].get_helper().nth_coord_ticks=0
+    # let bottom axis shows ticklabels for 2nd coordinate (radius)
+    ax1.axis["bottom"].get_helper().nth_coord_ticks=1
+
+    ax2 = ParasiteAxesAuxTrans(ax1, tr, "equal")
+
+    ax1.parasites.append(ax2)
+
+    return ax1,ax2
+
+class MilkywayPlot(object):
+
+    def __init__(self, figure, galrad=15.0, earthrad=8.5, npix=1024):
+
+        self.kpc_per_pix = galrad/(npix/2.0)
+        self.center = npix/2. - 0.5
+        self.earthxpos = earthrad / self.kpc_per_pix
+        self.earthrad = earthrad
+        self.galrad = galrad
+        self.npix = npix
+
+        self.center = self.npix/2. - 0.5
+
+        self.figure = figure
+
+        self.setup_milkyway_plot()
+
+    def setup_milkyway_plot(self):
+
+        ax1,ax2 = make_polar_axis(self.figure)
+        self.figure.add_subplot(ax1)
+
+        ax1.set_aspect(1.)
+        ax1.set_xlim(-1*self.earthxpos*self.kpc_per_pix,(self.npix-self.earthxpos)*self.kpc_per_pix)
+        ax1.set_ylim(-1*(self.npix/2.)*self.kpc_per_pix,(self.npix/2.)*self.kpc_per_pix)
+        ax1.set_xlim(-1*self.earthxpos*self.kpc_per_pix,(self.npix-self.earthxpos)*self.kpc_per_pix+5)
+        ax1.set_ylim(-1,(self.npix/2.)*self.kpc_per_pix)
+
+        #dropboxdeletedthis OKim = OKzone.astype('int')+OKgal.astype('int')
+        #ax1.imshow(OKim[(npix/2.)-250:,:],extent=[(-1*earthxpos)*kpc_per_pix,(npix-earthxpos)*kpc_per_pix,-250*kpc_per_pix,(npix/2.)*kpc_per_pix],cmap=matplotlib.cm.gray)
+        #plt.plot(linspace(-pi,pi,1000),
+        ax1.add_artist(Circle([self.earthrad,0],self.galrad,facecolor='gray',edgecolor='none',zorder=2))
+        #ax1.add_artist(Wedge([0,0],maxdist,0,90,facecolor='white',edgecolor='none'))
+
+        self.ax1,self.ax2 = ax1,ax2
+
+    def mark_completeness_zone(self, maxdist=17.5, mindist=5, low_longitude_cutoff=6):
+        yy,xx = np.indices([self.npix,self.npix])
+        rr = np.sqrt((xx-self.center)**2+(yy-self.center)**2)*self.kpc_per_pix
+        theta = np.arctan2(xx - self.center, yy - self.center)
+
+        x1 = 0
+        y1 = (self.galrad**2 - self.earthrad**2)**0.5
+        #x2 = (galrad**2-distance_cut**2-earthrad**2) / (-2*distance_cut*earthrad) * distance_cut
+        #y2 = (maxdist**2 - x2**2)**0.5
+        anglecut = low_longitude_cutoff/180.0 * np.pi
+        x3 = np.cos(anglecut) * maxdist
+        y3 = np.sin(anglecut) * maxdist
+        if self.galrad+self.earthrad > maxdist:
+            th2 = np.arccos(-1*(self.galrad**2-maxdist**2-self.earthrad**2)/(2*maxdist*self.earthrad))
+            x2 = np.cos(th2)*maxdist
+            y2 = np.sin(th2)*maxdist
+        else:
+            # daaaang geometry
+            x2 = x3 = (2*self.earthrad + ((2*self.earthrad)**2-4*(np.sin(anglecut)**2+1)*(self.earthrad**2-self.galrad**2))**0.5) / (2*(np.sin(anglecut)**2+1))
+            y2 = y3 = np.sin(anglecut)*(x2)
+            maxdist = (x3**2+y3**2)**0.5
+
+        x4 = 0
+        y4 = mindist
+        x5 = np.cos(anglecut) * mindist
+        y5 = np.sin(anglecut) * mindist
+        xx1 = np.concatenate([np.linspace(0,x2,1000), np.zeros(1000)])
+        xx2 = np.concatenate([np.zeros(1000), np.linspace(x2,x3,1000)])
+        xx = xx1+xx2
+        obs_curve = (self.galrad**2-(xx-self.earthrad)**2)**0.5 *(xx<=x2)  + (maxdist**2-xx**2)**0.5 * (xx>x2)
+        low_curve = (mindist**2-(xx*(xx<=x5))**2)**0.5 * (xx<=x5) + xx*np.sin(anglecut) * (xx>x5)
+        self.ax1.fill_between(xx,low_curve,obs_curve,facecolor='white',edgecolor='none',zorder=3)
+        #print "Vertices: " + "\nVertices: ".join(["%f,%f" % (x,y) for (x,y) in zip((x1,x2,x3,x4,x5),(y1,y2,y3,y4,y5))])
+
+        xxclose = np.linspace(0,x5,1000)
+        high_curve2 = (mindist**2-(xxclose)**2)**0.5 
+        low_curve2  = xxclose*np.sin(anglecut)
+        self.ax1.fill_between(xxclose, low_curve2, high_curve2, facecolor='#BBBBBB', edgecolor='none', zorder=4)
+
+
+    def mark_earth(self):
+        self.ax2.plot(0,0,'o',color='gold',markersize=10,markeredgecolor='gold',markerfacecolor='none',zorder=50,alpha=1,markeredgewidth=2)
+        self.ax2.plot(0,0,'.',color='gold',markersize=3,markeredgecolor='gold',markerfacecolor='gold',zorder=50,alpha=1,markeredgewidth=2)
+
+    def mark_gc(self):
+        self.ax2.plot(0,8.5,'o',color=(0.3,1,0.3,0.5),markersize=30,markeredgecolor=(0.3,1,0.3,0.5))
+        self.ax2.text(0,8.5,'GC',horizontalalignment='center',verticalalignment='center')
+
+    def label(self):
+        # this results in weird duplication ax1.set_xlabel("Heliocentric Distance (kpc)")
+        self.ax1.axis['bottom'].label.set_text("Heliocentric Distance (kpc)")
+        self.ax1.set_ylabel("Heliocentric Distance (kpc)")
+        # label.set_text http://matplotlib.sourceforge.net/examples/axes_grid/demo_floating_axes.html
+        self.ax1.axis['top'].label.set_text("Galactic Longitude")
+        self.ax2.axis['top'].label.set_text("Galactic Longitude")
+        self.ax1.axis['right'].label.set_text("Galactic Longitude")
+        self.ax2.axis['right'].label.set_text("Galactic Longitude")
+        self.ax1.axis['top'].label.set_visible(True)
+        self.ax2.axis['top'].label.set_visible(True)
+        self.ax1.axis['right'].label.set_visible(True)
+        self.ax2.axis['right'].label.set_visible(True)
+
+
+    def add_grid(self):
+        self.ax1.grid(True,zorder=11)
+
+    def mark_corotation(self):
+        self.ax1.add_artist(Circle([8.5,0],8.5,facecolor='none',edgecolor='r',linestyle='dotted',zorder=5))
+
+    def do_everything(self, **kwargs):
+        self.mark_earth()
+        self.mark_gc()
+        self.label()
+        self.add_grid()
+        self.mark_corotation()
+        self.mark_completeness_zone(**kwargs)
